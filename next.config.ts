baceeedfdd2c5fda012f5e420bc842f63f131553
Sterbike <hodiricsi@gmail.com>
2025-08-02 23:07:30 +0200

    assetPrefix added

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
+basePath: isProd ? "/portfolio_test" : "",
+assetPrefix: isProd ? "/portfolio_test/" : "",
 output: "export",
 distDir: "dist",
 images: {
